e63c37dd030918408f317f04152345e1d5f059d6
Michelle Wong <ellemichwong@hotmail.com>
2024-10-08 16:25:50 +1100

    Command now works across multiple audio chunks

--- audio_processing.py
+++ audio_processing.py
@@ -15,9 +15,8 @@ def whisper_send_and_receive(client, audio_chunk):
     translation = client.audio.translations.create(
         model="whisper-1", 
         file=audio_chunk,
-        response_format="text",
-        prompt="you are a helpful assistant for a computer science university group project your task is to correct any spelling discrepancies in the transcribed text and make sure the phrases seperated with z can be recognised z hey doc light on z hey doc light off z stop z change it to these outputs if the words sound even remotely like it could be any of these phrases since false positives are better than false negatives dont add your own output delete all common filler words",
+        temperature=0,
     )
 
     #return process_response(client, translation['text'])
-    return translation
+    return translation.text
