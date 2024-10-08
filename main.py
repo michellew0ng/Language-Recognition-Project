6374e63ac12cc8cfd6171413d526d248de08af34
Michelle Wong <ellemichwong@hotmail.com>
2024-10-08 11:14:15 +1100

    Changed output to prints, templated input from mic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from axi_interface import stream_audio_from_axi
+from axi_interface import stream_audio_from_mic()
 
 if __name__ == "__main__":
     print("Starting...the Marvellous Voice Activated LED!")
-    stream_audio_from_axi()
+    stream_audio_from_mic()
     
     
