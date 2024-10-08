d804da0c0d5ce3381707ea454412d43b2bb4e7a0
Michelle Wong <ellemichwong@hotmail.com>
2024-10-08 15:37:00 +1100

    Clean up

--- post_processing.py
+++ post_processing.py
@@ -5,13 +5,11 @@ from openai import OpenAI
 # ISSUE: What if the key phrase is caught over 2 audio chunks? How to solve? Must keep an external buffer to store this.
 def scan_for_key_phrase(transcription):
   
-    key_phrase_on = 'Hey Sam Light On'
-    key_phrase_off = 'Hey Sam Light Off'
+    key_phrase_on = 'Hey Doc Light On'
+    key_phrase_off = 'Hey Doc Light Off'
 
     if transcription:
         if key_phrase_on.lower() in transcription.lower():
             light_switch(True)
         elif key_phrase_off.lower() in transcription.lower():
             light_switch(False)
-
-    
