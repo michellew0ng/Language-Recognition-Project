3d5714886e9a4e8d5aac1ac54e0b767a327d9784
Michelle Wong <ellemichwong@hotmail.com>
2024-10-10 22:50:01 +1100

    Full light dimmer over x seconds functionality implemented + some organisation

--- light_timer.py
+++ light_timer.py
@@ -5,10 +5,13 @@ import threading
 timer_thread = None
 timer_active = False
 
+dimmer_active = False
+dim_threads = []
+
 # activates led on, waits the given number of seconds, then led off
 # works for up to 999 seconds 
 def timer_start(num_secs):
-    global timer_thread, timer_active, previous_brightness 
+    global timer_thread, timer_active
 
     previous_brightness = hardware_control.get_current_brightness()
 
@@ -38,3 +41,39 @@ def timer_cancel():
         timer_active = False
     else:
         print("No active timer to cancel.")
+
+# Dims the lights from full brightness to off over the course of num_secs
+def timer_dim(num_secs):
+    global dimmer_active
+    dimmer_active = True
+    interval = num_secs / 5
+    
+    def set_brightness(level):
+        global dimmer_active
+        if level >= 0 and dimmer_active:
+            hardware_control.light_switch(level)
+            print(f"Brightness dimmed to level {level}")
+            # Schedule the next brightness change
+            if level > 0:
+                thread = threading.Timer(interval, set_brightness, [level - 1])
+                thread.start()
+                dim_threads.append(thread)
+            else:
+                print(f"Light is completely dimmed. Goodnight!")
+
+
+    print(f"Dimming the lights over {num_secs} seconds.")
+    set_brightness(hardware_control.LED_LEVEL_5)
+
+def timer_dim_cancel():
+    global dimmer_active
+    
+    if dimmer_active:
+        dimmer_active = False
+        # Cancel all active dimming timers
+        for thread in dim_threads:
+            thread.cancel()
+        dim_threads.clear()  # Clear the list of threads 
+    else:
+        print("No dimming in progress to cancel.")
+
