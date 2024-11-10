c5d09993587bed0b62f0dcc3ebea89caf04eb5f8
Hasitha <hasitha.peter@gmail.com>
2024-11-10 22:39:49 +1100

    adding files for threads implemenation to run process_signal and main_m5 simultaneously

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+
+
+// Declarations of external functions
+int process_signal();
+int main_m5_task();
+
+void* thread1_func(void* arg) {
+    process_signal();
+    return NULL;
+}
+
+void* thread2_func(void* arg) {
+    main_m5_task();
+    return NULL;
+}
+
+int main() {
+    pthread_t thread1, thread2;
+
+    // Create threads
+    pthread_create(&thread1, NULL, thread1_func, NULL);
+    pthread_create(&thread2, NULL, thread2_func, NULL);
+
+    // Wait for threads to complete
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Threads have completed.\n");
+    return 0;
+}
