783878830e6ea882c298d58151abd62fb00bbfeb
Michelle Wong <ellemichwong@hotmail.com>
2024-11-12 19:00:02 +1100

    Fixed whisper issue, added server and client test files

--- voice_recognition/axi_interface.py
+++ voice_recognition/axi_interface.py
@@ -1,9 +1,12 @@
 import socket
 import speech_recognition as sr
 import io
+import signal
 import time
 import queue
 import wave
+import sys
+
 from openai import OpenAI
 import threading
 from audio_processing import whisper_send_and_receive
@@ -43,7 +46,8 @@ def stream_audio_from_mic(chunk_duration=3):
                 wav_data = audio.get_wav_data()
                 wav_file = io.BytesIO(wav_data)
                 wav_file.name = "audio.wav"
-
+                wav_file.seek(0);
+                
                 transcription = whisper_send_and_receive(client, wav_file)
                 scan_for_key_phrase(transcription)
             
@@ -62,25 +66,13 @@ def stream_audio_from_mic(chunk_duration=3):
 ##### SOLN 2: Recieves audio from AXI bus ####
 # Uses threading to account for dropped packets
 
-def save_buffer_as_wav(buffer, sample_rate=44100, num_channels=2, sample_width=2):
-    wav_data = io.BytesIO()
-    with wave.open(wav_data, 'wb') as wav_file:
-        wav_file.setnchannels(num_channels)
-        wav_file.setsampwidth(sample_width)
-        wav_file.setframerate(sample_rate)
-        wav_file.writeframes(buffer)
-
-    wav_data.seek(0)
-
-    return wav_data
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((SERVER_IP, ETHERNET_PORT))
 data_queue = queue.Queue(maxsize=QUEUE_SIZE)
 LED_ON_SIGNAL = 'T'
 LED_OFF_SIGNAL = 'F'
 
-def rcv_audio_data():
+stop_event = threading.Event()
+
+def rcv_audio_data(sock):
     while True:
         print("I AM CURRENTLY RECIEVING")
         try:
@@ -106,16 +98,17 @@ buffer = bytearray()
 
 # Process audio data from the queue
 def process_audio_data():
-    while True:
+    while not stop_event.is_set():
         print("i am processing the audio now")
         try:
             print("getting the data")
             data = data_queue.get()
 
-            if b'END' in data:
+            if data == b'END':
                 if buffer:
-                    response = whisper_send_and_receive(save_buffer_as_wav(buffer))
-                    print("I have sent to whisper")
+                    conv_audio = io.BytesIO(buffer)
+                    conv_audio.seek(0)
+                    response = whisper_send_and_receive(client, conv_audio)
                     scan_for_key_phrase(response)
                     print("I tried to scan for a key phrase")
                     buffer.clear()
@@ -128,22 +121,34 @@ def process_audio_data():
 
 # Initialises threads and streams audio from axi bus
 def stream_audio_from_axi():
-    receiver_thread = threading.Thread(target=rcv_audio_data)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((SERVER_IP, ETHERNET_PORT))
+    
+    receiver_thread = threading.Thread(
+        target=rcv_audio_data, kwargs={'sock': sock}
+    )
     receiver_thread.start()
 
     processor_thread = threading.Thread(target=process_audio_data)
     processor_thread.start()
 
-    monitoring_thread = threading.Thread(target=monitor_led_state)
+    monitoring_thread = threading.Thread(
+        target=monitor_led_state, kwargs={'sock': sock}
+    )
     monitoring_thread.start()
 
-    receiver_thread.join()
-    processor_thread.join()
-    monitoring_thread.join()
+    try:
+        receiver_thread.join()
+        processor_thread.join()
+        monitoring_thread.join()
+    except KeyboardInterrupt:
+        print("\nShutting down...")
+        stop_event.set()
+        sys.exit(0)
 
 # Monitors LED state and sends signal when changes
-def monitor_led_state():
-    while True:
+def monitor_led_state(sock):
+    while not stop_event.is_set():
         if led_state.need_to_change is True:
             if led_state.led_state == led_state.LED_OFF:
                 message = LED_OFF_SIGNAL
@@ -156,9 +161,22 @@ def monitor_led_state():
         time.sleep(0.5)
 
 
+# Implements the signal for CTRL+C to kill the program
+def signal_handler(sig, frame):
+    print("\nCaught SIGINT (Ctrl+C), shutting down gracefully...")
+    stop_event.set()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+############### TESTS ###################
+
 # Turns light on and off every 10 seconds
 def test_send_data():
-    while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((SERVER_IP, ETHERNET_PORT))
+
+    while not stop_event.is_set():
         try:
             message = LED_ON_SIGNAL
             sock.sendto(message.encode(), (SERVER_IP, ETHERNET_PORT))
